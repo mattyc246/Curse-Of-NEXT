ab87c5a91ae61bc55261558d1c8a39f7067c016c
Matthew Cross <mattyc246@gmail.com>
2019-10-28 16:45:27 +0800

    added link to submit form

--- src/pages/EndPage.js
+++ src/pages/EndPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import MainContainer from "../containers/MainContainer";
 import useStores from "../hooks/useStores";
 
@@ -6,21 +6,41 @@ const EndPage = () => {
   const {
     gameStore: { stopTimer, minutes, seconds }
   } = useStores();
+
+  const [gFormUrl, setFormUrl] = useState(
+    "https://docs.google.com/forms/d/e/1FAIpQLSd6JYOdjsO-ejXcp0-zilcL649aPj8jepFqrgqlcwU4n2P67Q/viewform?usp=pp_url&entry.1158048600="
+  );
+
   useEffect(() => {
     stopTimer();
+    let newFormUrl = `${gFormUrl}${minutes}mins+${seconds}secs`;
+    setFormUrl(newFormUrl);
   }, []);
+
+  const submitDetails = () => {
+    window.open(gFormUrl);
+  };
+
   return (
     <MainContainer>
       <h2 className="font-main text-orange big-font text-center">
         Congratulations!
       </h2>
-      <h4 className="font-alt text-orange">
-        You completed the game in <strong>{minutes} minutes</strong> and{" "}
-        <strong>{seconds} seconds</strong>!
-      </h4>
-      <p className="font-alt text-orange">
-        Leave your details below to stand a chance to win 2 tickets to Hauntu.
-      </p>
+      <div className="w-50">
+        <h4 className="font-alt text-orange text-center">
+          You completed the game in <strong>{minutes} minutes</strong> and{" "}
+          <strong>{seconds} seconds</strong>!
+        </h4>
+        <p className="font-alt text-orange text-center">
+          Leave your details below to stand a chance to win 2 tickets to Hauntu.
+        </p>
+      </div>
+      <button
+        onClick={() => submitDetails()}
+        className="btn-orange text-orange font-alt mx-auto d-block"
+      >
+        Open Form
+      </button>
     </MainContainer>
   );
 };
